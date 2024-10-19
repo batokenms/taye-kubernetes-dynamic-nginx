77a978d026dd6733e67f9f9b0224f0dccece2dd4
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-10-19 07:46:06 -0400

    Update reload-nginx.sh

--- reload-nginx.sh
+++ reload-nginx.sh
@@ -8,3 +8,9 @@ while inotifywait -e modify /usr/share/nginx/html/index.html; do
   echo "ConfigMap content changed, reloading NGINX..."
   nginx -s reload
 done
+
+# Build the Docker image and tag it
+# docker build -t your-registry/nginx-dynamic:latest .
+# Remember to use the bash script that we used to automate the process of building and pushing the image to the docker registry
+# Here the re[pository with the script 
+# https://github.com/joshking1/image-to-be-deployed.git
